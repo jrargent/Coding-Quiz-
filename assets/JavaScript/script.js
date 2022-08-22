1fa304ff43bed18e24429ac51e831503309bd03d
Argent <jrargent42@gmail.com>
2022-08-21 21:19:10 -0400

    add timer, test countdown

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,4 +1,4 @@
-var timeLeft = 100;
+var timeLeft = 10;
 var timerEl = document.querySelector("#timer");
 
 
@@ -6,27 +6,31 @@ var timerEl = document.querySelector("#timer");
 
 // Timer that counts down
 function countdown() {
-    timerEl.textContent = "Time:";
-}
+    //set default start time here? or have this return something?
+    
+
+
 
     
-// // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-// var timeInterval = setInterval(function () {
-//     // As long as the `timeLeft` is greater than 1
-//     if (timeLeft > 1) {
-//       // Set the `textContent` of `timerEl` to show the remaining seconds
-//       timerEl.textContent = "Time:" + timeLeft;
-//       // Decrement `timeLeft` by 1
-//       timeLeft--;
-//     } else {
-//       // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//       timerEl.textContent = '';
-//       // Use `clearInterval()` to stop the timer
-//       clearInterval(timeInterval);
-//       // Call the function to end quiz
+// Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 0) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = "Time: " + timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = "Time: 0";
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+
+      // Call the function to end quiz
+      
      
-//     }
-//   }, 1000);
-// };
+    }
+  }, 1000);
+};
 
 countdown();
